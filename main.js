163cb90e7ad4d15f3a23f59af7861ba93e45084c
Raphael Serota <raphael@codeplatoon.org>
2022-02-06 16:35:32 -0600

    full of bugs

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-let talking = true
-let goodbyes = 0
+let continue = true
 
-while ( talking ) {
-    let statement = prompt()
-    if ( statement === '' ) {
-        alert('WHAT!?')
-    }
-    else if ( statement.toUpperCase() !== statement ) {
-        alert('SPEAK UP, KID!')
-    }
-    else if ( statement === 'GOODBYE!' ) {
-        goodbyes++
+while ( continue ) {
 
-        if ( goodbyes === 1 ) {
-            alert('LEAVING SO SOON?')
-        }
-        else if ( goodbyes === 2 ) {
-            alert('LATER, SKATER!')
-            talking = false
-        }
+    let goodbyes = 0
+    let userInput = prompt()
+
+    if ( userInput = "" ) {
+        alert("WHAT!?")
     }
-    else if ( statement.toUpperCase() === statement ) {
+    else if ( userinput.toUppercase != statement ) {
+        alert("SPEAK UP, KID!")
+    }
+    else if ( statement.toUppercase == statement ) {
         alert('NO, NOT SINCE 1946!')
+    }
+    else if ( userinput == "GOODBYE!" ) {
+        goodbyes + 1
+
+        if ( goodbyes == 1 ) {
+            alert("LEAVING SO SOON?')
+        }
+        else if ( goodbyes == 2 ) {
+            alert("LATER, SKATER!")
+            continue = false
+        }
     }
 }
