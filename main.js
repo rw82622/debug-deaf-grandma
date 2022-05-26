f80ce526a25a9cdf732064a6bb7c980894916a00
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-26 16:51:31 -0500

    deaf grandma successfully debugged

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-let continue = true
+function deafGrandma() {
+  let keepRunning = true;
+  let goodbyes = 0;
 
-while ( continue ) {
+  while (keepRunning) {
+    let userInput = prompt("");
 
-    let goodbyes = 0
-    let userInput = prompt()
+    if (userInput === "") {
+      alert("WHAT!?");
+    } else if (userInput.toUpperCase() != userInput) {
+      alert("SPEAK UP, KID!");
+    } else if (userInput === "GOODBYE!") {
+      goodbyes += 1;
 
-    if ( userInput = "" ) {
-        alert("WHAT!?")
+      if (goodbyes === 1) {
+        alert("LEAVING SO SOON?");
+      } else if (goodbyes === 2) {
+        alert("LATER, SKATER!");
+        keepRunning = false;
+      }
+    } else if (userInput.toUpperCase() === userInput) {
+      alert("NO, NOT SINCE 1946!");
     }
-    else if ( userinput.toUppercase != statement ) {
-        alert("SPEAK UP, KID!")
-    }
-    else if ( statement.toUppercase == statement ) {
-        alert('NO, NOT SINCE 1946!')
-    }
-    else if ( userinput == "GOODBYE!" ) {
-        goodbyes + 1
-
-        if ( goodbyes == 1 ) {
-            alert("LEAVING SO SOON?')
-        }
-        else if ( goodbyes == 2 ) {
-            alert("LATER, SKATER!")
-            continue = false
-        }
-    }
+  }
 }
+
+deafGrandma();
